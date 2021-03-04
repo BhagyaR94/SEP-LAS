fe04f9edb8625b7f18dd86d8394d08fdd69ad2f9
hasara <hasara20abeywadhane@gmail.com>
2021-03-04 11:38:29 +0530

    leave form frontend

---
+++ resources/lang/si/leave.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'Leave Form Akuressa Maha Vidyalaya' => '',
+    'name' => 'නම',
+    'designation' => 'තනතුර',
+    'department' => 'ෙදපාර්තෙම්න්තුෙව්',
+    'leave_type' => 'නිවාඩු වර්ගය',
+    'casual_leave' => 'අනියම් නිවාඩු',
+    'sick_leave' => 'ලෙඩ නිවාඩු',
+    'other' => 'අනික්',
+    'date' => 'දිනය',
+    'Number_Of_Days_Apply' => 'අයදුම් කරන දින ගණන',
+    'Reason' => 'හේතුව',
+    'First_Appoinment_Date' => 'පළමු පත්වීම් දිනය',
+    'Reserved_Person_Name' => 'වෙන් කළ පුද්ගලයාගේ නම',
+    'Address_When_On_Leave' => 'නිවාඩු ඇති විට ලිපිනය',
+    'Tel_No' => 'දුරකථන අංකය',
+    'submit' => 'ඉදිරිපත් කරන්න',
+    'submit1' => 'අවලංගු කරන්න',
+
+
+];
