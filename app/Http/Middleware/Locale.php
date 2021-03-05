9808a17aeb446942d6fc9ee33d38f0e868841dcf
Bhagya Rathnayake <rathnayake.bhagya94@gmail.com>
2021-03-06 00:35:35 +0530

    Bug with app locale resolved.

---
+++ app/Http/Middleware/Locale.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+
+class Locale
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        App::setLocale(session('session'));
+        return $next($request);
+    }
+}
