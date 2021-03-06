5b61f8a0a57eec94508755a7b0741813a2811998
Kaushalya <kgkabeywardhane@gmail.com>
2021-03-06 21:07:40 +0530

    sign_up_backend

--- app/Http/Controllers/SignUpController.php
+++ app/Http/Controllers/SignUpController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\App;
+use App\Models\User;
 
 class SignUpController extends Controller
 {
-    //
+  public function storeDataDb(Request $request)
+  {
+   
+    $this->validate($request, [
+      'username' => 'required|min:3|max:50',
+      'email' => 'email',
+      'password' => 'required|confirmed|min:6',
+  ]);
+
+    $user = new User();
+    $user->name = $request->input('username');
+    $user->email = $request->input('email');
+    $user->password = $request->input('password');
+    $user->user_type = 1;
+    $user->save();
+  }
 }
