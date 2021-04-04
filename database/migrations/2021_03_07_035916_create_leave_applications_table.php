728baa997b26b90cc50028d33db8d5e2d6846e0e
Kaushalya <kgkabeywardhane@gmail.com>
2021-04-04 12:00:09 +0530

    leave application intergration

--- database/migrations/2021_03_07_035916_create_leave_applications_table.php
+++ database/migrations/2021_03_07_035916_create_leave_applications_table.php
@@ -19,19 +19,21 @@ class CreateLeaveApplicationsTable extends Migration
             $table->timestamps();
             $table->date('start_date')->nullable(false);
             $table->date('end_date')->nullable(false);
-            $table->string('reason',225)->nullable(false);
-            $table->char('type',120)->nullable(false);
-            $table->enum('half_day', ['morning','evening']);       
+            $table->integer('number_of_days')->nullable(false);
+            $table->string('reason', 225)->nullable(false);
+            $table->string('leave_type', 50)->nullable(false);
+            // $table->enum('half_day', ['morning','evening']);       
             $table->string('contact_location')->nullable(false);
-            $table->enum('status',['pending','approved','rejected'])->nullable(false);
-            $table->integer('substitute_employee_id');
-            $table->integer('supervisor_employee_id')->nullable(false);
-            
+            $table->integer('telephone', 11)->nullable(true);
+            $table->enum('status', ['pending', 'approved', 'rejected'])->nullable(false);
+            $table->integer('substitute_employee_id')->nullable(true);
+            $table->integer('supervisor_employee_id')->nullable(true);
+
             //constraints 
             //$table->primary('id');
-            $table->unique( ['applicant_id', 'start_date', 'end_date' ]);
-         #   $table->foreign('substitute_employee_id')->references('id')->on('employee');
-         #   $table->foreign('supervisor_employee_id')->references('id')->on('employee');           
+            // $table->unique( ['applicant_id', 'start_date', 'end_date' ]);
+            #   $table->foreign('substitute_employee_id')->references('id')->on('employee');
+            #   $table->foreign('supervisor_employee_id')->references('id')->on('employee');           
         });
     }
 
--- resources/lang/en/leave.php
+++ resources/lang/en/leave.php
@@ -14,23 +14,24 @@ return [
     */
 
     'Leave_Form_Akuressa_Maha_Vidyalaya' => 'Leave Form Akuressa Maha Vidyalaya',
-    'name' => 'Name',
-    'designation' => 'Designation',
-    'department' => 'Depatment',
+    //'name' => 'Name',
+    //'designation' => 'Designation',
+    //'department' => 'Depatment',
     'leave_type' => 'Leave Type',
     'casual_leave' => 'casual leave',
     'sick_leave' => 'sick leave',
     'other' => 'other',
-    'date' => 'Date',
+    'startdate' => 'Leave Start Date',
+    'enddate' => 'Leave End Date',
     'Number_Of_Days_Apply' => 'Number Of Days Apply',
     'Reason' => 'Reason',
-    'First_Appoinment_Date' => 'First Appoinment Date',
-    'Reserved_Person_Name' => 'Reserved Person Name',
     'Address_When_On_Leave' => 'AddressWhen On Leave',
     'Tel_No' => 'Telephone Number',
+    //'First_Appoinment_Date' => 'First Appoinment Date',
+    'Backup_Person_Name' => 'Backup Person Name',
+    'Leave_Status' => 'Leave_Status',
     'submit' => 'Submit',
     'submit1' => 'Cancel',
 
 
 ];
-
