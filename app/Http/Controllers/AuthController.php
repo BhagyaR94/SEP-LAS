ae58e0114dadb6fbc0a3ef61b0842424a44f1479
Bhagya Rathnayake <rathnayake.bhagya94@gmail.com>
2021-05-02 12:41:15 +0530

    read me md file modified

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Session;
 
 class AuthController extends Controller
